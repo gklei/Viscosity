6c9d9c05a3044042dbb17d5743c9c050120d770b
Gregory Klein <gregoryhklein@gmail.com>
2014-05-09 12:49:17 -0400

    Added new image and file for velocity node.

---
+++ Viscosity/VISCVelocityNode.h
@@ -0,0 +1,13 @@
+//
+//  VISCVelocityNode.h
+//  Viscosity
+//
+//  Created by Klein, Greg on 5/9/14.
+//  Copyright (c) 2014 HardFlip, LLC. All rights reserved.
+//
+
+#import <SpriteKit/SpriteKit.h>
+
+@interface VISCVelocityNode : SKSpriteNode
+
+@end
---
+++ Viscosity/VISCVelocityNode.m
@@ -0,0 +1,13 @@
+//
+//  VISCVelocityNode.m
+//  Viscosity
+//
+//  Created by Klein, Greg on 5/9/14.
+//  Copyright (c) 2014 HardFlip, LLC. All rights reserved.
+//
+
+#import "VISCVelocityNode.h"
+
+@implementation VISCVelocityNode
+
+@end
