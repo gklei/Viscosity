c1f19b6cd0cda4e0604793329453fcab650d20bb
Gregory Klein <gregoryhklein@gmail.com>
2014-05-11 23:31:07 -0400

    Added files for directional animating node.

---
+++ Viscosity/VISCAnimatingDirectionalNode.m
@@ -0,0 +1,13 @@
+//
+//  VISCAnimatingDirectionalNode.m
+//  Viscosity
+//
+//  Created by Gregory Klein on 5/11/14.
+//  Copyright (c) 2014 HardFlip, LLC. All rights reserved.
+//
+
+#import "VISCAnimatingDirectionalNode.h"
+
+@implementation VISCAnimatingDirectionalNode
+
+@end
