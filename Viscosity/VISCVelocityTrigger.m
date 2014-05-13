0f7a3127ebb9c0ad93eb6f8051a467ce7a267cb0
Gregory Klein <gregoryhklein@gmail.com>
2014-05-12 20:34:20 -0400

    Added files for velocity trigger.

---
+++ Viscosity/VISCVelocityTrigger.m
@@ -0,0 +1,13 @@
+//
+//  VISCVelocityTrigger.m
+//  Viscosity
+//
+//  Created by Gregory Klein on 5/12/14.
+//  Copyright (c) 2014 HardFlip, LLC. All rights reserved.
+//
+
+#import "VISCVelocityTrigger.h"
+
+@implementation VISCVelocityTrigger
+
+@end
